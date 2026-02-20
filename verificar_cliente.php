8395ef34fb716304f1438f7b953f67dc4ef25b06
TopMasterTV <topmasteronlinetv@gmail.com>
2026-02-20 18:57:58 -0300

    Update verificar_cliente.php

--- verificar_cliente.php
+++ verificar_cliente.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-require_once _DIR_ . "/db.php";
+require_once __DIR__ . "/db.php";
 
 $cliente_id = $_GET['cliente_id'] ?? $_POST['cliente_id'] ?? '';
 
